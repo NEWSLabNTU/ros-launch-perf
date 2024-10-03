afc1289c2767a3c05a60e5ac76e77f3f12c94340
aeon <jerry73204@gmail.com>
2024-10-04 05:07:15 +0800

    Support `package_name + launch_file_name` syntax in dump_launch.

--- dump_launch/src/dump_launch/__init__.py
+++ dump_launch/src/dump_launch/__init__.py
@@ -10,17 +10,42 @@ from typing import Tuple  # noqa: F401
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import AnyLaunchDescriptionSource
+from ros2launch.api import get_share_file_path_from_package
+from ros2launch.api import MultipleLaunchFilesError
+from ament_index_python.packages import PackageNotFoundError
 
 from .inspector import LaunchInspector
 
 
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument("LAUNCH_FILE")
-    parser.add_argument("LAUNCH_ARGS", nargs="*")
+    parser.add_argument("package_name")
+    parser.add_argument("launch_file_name", nargs="?")
+    parser.add_argument("launch_arguments", nargs="*")
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("-o", "--output", default="record.json")
     args = parser.parse_args()
+
+    launch_arguments = list()
+    if os.path.isfile(args.package_name):
+        launch_path = args.package_name
+        if args.launch_file_name is not None:
+            # Since in single file mode, the "launch file" argument is
+            # actually part of the launch arguments, if set.
+            launch_arguments.append(args.launch_file_name)
+    else:
+        try:
+            launch_path = get_share_file_path_from_package(
+                package_name=args.package_name, file_name=args.launch_file_name
+            )
+        except PackageNotFoundError as exc:
+            raise RuntimeError(
+                "Package '{}' not found: {}".format(args.package_name, exc)
+            )
+        except (FileNotFoundError, MultipleLaunchFilesError) as exc:
+            raise RuntimeError(str(exc))
+
+    launch_arguments.extend(args.launch_arguments)
 
     output_file = args.output
     if os.path.exists(output_file):
@@ -34,14 +59,14 @@ def main() -> int:
                 nth += 1
 
     inspector = LaunchInspector(
-        argv=args.LAUNCH_ARGS, noninteractive=True, debug=args.debug
+        argv=launch_arguments, noninteractive=True, debug=args.debug
     )
 
-    parsed_args = parse_launch_arguments(args.LAUNCH_ARGS)
+    parsed_args = parse_launch_arguments(launch_arguments)
     launch_description = LaunchDescription(
         [
             IncludeLaunchDescription(
-                AnyLaunchDescriptionSource(args.LAUNCH_FILE),
+                AnyLaunchDescriptionSource(launch_path),
                 launch_arguments=parsed_args,
             )
         ]
