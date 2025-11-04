afa805906b1d453d943fb2aeab14c061b50b1f5f
aeon <jerry73204@gmail.com>
2025-11-04 16:35:56 +0800

    Add /opt/ros/humble/lib to RPATH in compiled binaries

--- src/play_launch/build.rs
+++ src/play_launch/build.rs
@@ -6,4 +6,9 @@ fn main() {
     println!("cargo:rustc-link-lib=rcl_interfaces__rosidl_typesupport_c");
     println!("cargo:rustc-link-lib=rcl_interfaces__rosidl_generator_c");
     println!("cargo:rustc-link-lib=rosidl_runtime_c");
+
+    // Add RPATH for runtime library discovery (allows .deb package to work standalone)
+    // Use old-style RPATH (not RUNPATH) so it applies to transitive dependencies
+    println!("cargo:rustc-link-arg=-Wl,--disable-new-dtags");
+    println!("cargo:rustc-link-arg=-Wl,-rpath,/opt/ros/humble/lib");
 }
